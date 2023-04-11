e687abc8ff4a2b27ec3172142e4986a54fb145b9
koveshnikov93 <130228242+koveshnikov93@users.noreply.github.com>
2023-04-11 03:04:53 +0000

    Обновил кнопки

--- src/Components/SecondaryButton.js
+++ src/Components/SecondaryButton.js
@@ -1,13 +1,15 @@
 import Clickable from "./Clickable";
 import styled from "styled-components";
 
+
+
 function SecondaryButton(props) {
     // Здесь устанавливается минимальная ширина для кнопки
-    let minWidth = props.minWidth === undefined ? '100px' : props.minWidth;
+    let minWidth = props.minWidth === undefined ? '217px' : props.minWidth;
     const Button = styled.div `
-        background: #FFFFFF;
-        border: 1px solid #64748B;
-        border-radius: 24px;
+    background: #FFFFFF;
+    border: 1px solid #64748B;
+    border-radius: 24px;
         min-width: ${minWidth};
         color: #64748B;
         &:hover {
