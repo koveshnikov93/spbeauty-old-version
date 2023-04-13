ea7aa812b48cfaa1118c0837c25b8f670ee213fc
koveshnikov93 <130228242+koveshnikov93@users.noreply.github.com>
2023-04-13 01:25:16 +0000

    13.04

--- src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
@@ -5,7 +5,7 @@ function Addresses() {
         <div className='d-inline-flex gap-4 mt-4 border-adresses flex-column px-4 py-4 col justify-content-between'>
             <div className='d-flex flex-column gap-4'>
                 <div className='title px-2'>Адреса заведений</div>
-                <div className='d-inline-flex flex-column'>
+                <div className='d-inline-flex flex-column gray'>
                     <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
                     <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>
                 </div>
